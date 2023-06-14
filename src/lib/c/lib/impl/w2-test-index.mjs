cb5437cd7b8d92bc8905e2b7305baacdd4b253e0
Matthew <863968+mattbis@users.noreply.github.com>
2023-06-14 19:45:52 +0100

    Update w2-test-index.mjs

--- src/lib/c/lib/impl/w2-test-index.mjs
+++ src/lib/c/lib/impl/w2-test-index.mjs
@@ -3,6 +3,7 @@ function w2_test_i_keep() {}
 function w2_test_i_map() {}
 function w2_test_i_processor() {}
 function w2_test_i_main() {}
+function w2_test_i_main_retry() {}
 function w2_test_i_get() {}
 function w2_test_i_set() {}
 function w2_test_i_persist() {}
